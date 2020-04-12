5fa1b11db1deb3b9f0ef5eabc0d5932c0944caa3
Yannick <ydjoumbi@gmail.com>
2020-04-12 15:21:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,9 +6,9 @@ def using_insert(array,element)
   array.insert(4,element)
 end
 
-#def using_uniq(array)
- # array.uniq
-#end
+def using_uniq(array)
+  array.uniq
+end
 
 #def using_flatten(array)
  # array.flatten
